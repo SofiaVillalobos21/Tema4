d6cba28b1ba8df4503b63b67766c7c05f5f92884
SofiaVillalobos21 <93304238+SofiaVillalobos21@users.noreply.github.com>
2021-11-15 16:15:56 -0600

    Update L4.py

--- L4.py
+++ L4.py
@@ -6,8 +6,8 @@ import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
 
-# Variables aleatorias A y Z
-vaA = stats.norm(5, np.sqrt(0.2))
+# Variables aleatorias C, T y Z
+vaC = stats.norm(5, np.sqrt(0.2))
 vaZ = stats.uniform(0, np.pi/2)
 vaT= stats.uniform(2*np.pi*59.1, 2*np.pi*60.1)
 
@@ -22,10 +22,10 @@ X_t = np.empty((N, len(t)))	# N funciones del tiempo x(t) con T puntos
 
 # Creación de las muestras del proceso x(t) (A y Z independientes)
 for i in range(N):
-	A = vaA.rvs()
+	C = vaC.rvs()
 	Z = vaZ.rvs()
 	T = vaT.rvs()
-	x_t = A * np.cos(T*t + Z)
+	x_t = C * np.cos(T*t + Z)
 	X_t[i,:] = x_t
 	plt.plot(t, x_t)
 
